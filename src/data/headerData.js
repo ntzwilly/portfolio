b07c73ba774da1f0cc93bbe05b5fea8b45863a71
ntzwilly <ntzwilly@gmail.com>
2022-01-16 19:21:02 +0200

    Edit header

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import image from '../assets/jpg/photo.jpg';
 
 export const headerData = {
   name: 'Willy Ntazama',
-  title: 'Freelance full-stack Web Developer',
+  title: 'Freelance full-stack Developer',
   desciption: 'Full Stack Developer with eight years of experience in the ICT domain. Fluent in multiple languages, frameworks, and technologies, and capable of ramping up quickly and efficiently.',
   image,
   resumePdf: resume,
