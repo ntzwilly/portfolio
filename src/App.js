e0e6cfee0ba7dbf3818a0e03101f6dc970f00d42
ntzwilly <ntzwilly@gmail.com>
2022-01-16 06:20:15 +0200

    change package.json file

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ function App() {
           <Route path="/" exact component={Main} />
           <Route path="/blog" exact component={BlogPage} />
           <Route path="/projects" exact component={ProjectPage} />
-
           <Redirect to="/" />
         </Switch>
       </Router>
