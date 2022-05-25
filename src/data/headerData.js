3cc785a039f3ec02e752ff4e6d3144ef2c166722
ntzwilly <ntzwilly@gmail.com>
2022-05-25 20:18:34 +0200

    Fix typo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,7 +5,7 @@ import image from '../assets/jpg/photo.jpg';
 export const headerData = {
   name: 'Willy Ntazama',
   title: 'Freelance full-stack Developer',
-  desciption: 'Full Stack Developer with eight years of experience in the ICT domain. Fluent in multiple languages, frameworks, technologies, and capable of ramping up quickly and efficiently.',
+  desciption: 'Full Stack Developer with eight years of experience in the ICT domain. Fluent in multiple languages, frameworks, and technologies, and capable of ramping up quickly and efficiently.',
   image,
   resumePdf: resume,
 };
